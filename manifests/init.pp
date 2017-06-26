c2486a1720e108b8f95f71a6553659924ab643f1
Jeffrey Miller <millerjl1701@gmail.com>
2017-06-27 00:33:44 +0200

    translated what appear to be the needed variables for configuring galaxy from ansible-galaxy defaults/main.yml and added sensible defaults in line with defaults/main.yml and the README.md file... these could change depending on the outcome of the work with the rest of the playbooks

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,12 +11,27 @@
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
 class galaxy (
-  # parameters for the class will be here
+  Stdlib::Absolutepath                              $galaxy_server_dir            = '/opt/galaxy/server',
+  Stdlib::Absolutepath                              $galaxy_config_dir            = '/opt/galaxy/config',
+  Stdlib::Absolutepath                              $galaxy_config_file           = '/opt/galaxy/config/galaxy.ini',
+  Stdlib::Absolutepath                              $galaxy_mutable_config_dir    = '/var/opt/galaxy/config',
+  Stdlib::Absolutepath                              $galaxy_tool_dependency_dir   = '/opt/galaxy/server/dependencies',
+  Stdlib::Absolutepath                              $galaxy_venv_dir              = '/opt/galaxy/server/.venv',
+  Stdlib::Absolutepath                              $galaxy_mutable_data_dir      = '/var/opt/galaxy/data',
+  Stdlib::Absolutepath                              $galaxy_shed_tools_dir        = '/opt/galaxy/shed_tools',
+  Stdlib::Absolutepath                              $galaxy_shed_tool_conf_file   = '/var/opt/galaxy/config/shed_tool_conf.xml',
+  Stdlib::Absolutepath                              $galaxy_requirements_file     = '/opt/galaxy/server/lib/galaxy/dependencies/pinned-requirements.txt',
+  String                                            $galaxy_commit_id             = 'release_17.05',
+  String                                            $galaxy_repo_url              = 'https://github.com/galaxyproject/galaxy.git',
+  Boolean                                           $galaxy_manage_clone          = true,
+  Boolean                                           $galaxy_force_checkout        = false,
+  Boolean                                           $galaxy_manage_download       = false,
+  Boolean                                           $galaxy_manage_static_setup   = true,
+  Boolean                                           $galaxy_manage_mutable_setup  = true,
+  Boolean                                           $galaxy_manage_database       = true,
+  Boolean                                           $galaxy_fetch_dependencies    = true,
+  Boolean                                           $galaxy_manage_errordocs      = false,
 ) {
-
-  # validate parameters here
-
-
 
   case $::osfamily {
     'RedHat', 'Amazon': {

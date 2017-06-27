9ccb0c62fdda1a68558a14fbe282a3e783589990
Jeffrey Miller <millerjl1701@gmail.com>
2017-06-27 10:49:05 +0200

    spec tests: added test for git package and context for when galaxy_manage_git is set to false; added test for when galaxy_use_separate_users is set to false; added test for changing galaxy_base_dir to not be /opt/galaxy; added test for when changing galaxy_runtime_dir to not be /var/opt/galaxy

--- spec/classes/galaxy_spec.rb
+++ spec/classes/galaxy_spec.rb
@@ -32,6 +32,55 @@ describe 'galaxy' do
             'managehome' => 'true',
             'system'     => 'true',
           ) }
+
+          it { is_expected.to contain_package('git').with_ensure('present') }
+        end
+
+        context "galaxy class with galaxy_manage_git set to false" do
+          let(:params){
+            {
+              :galaxy_manage_git => false,
+            }
+          }
+          it { is_expected.to_not contain_package('git') }
+        end
+
+        context "galaxy class with galaxy_use_separate_users set to false" do
+          let(:params){
+            {
+              :galaxy_use_separate_users => false,
+            }
+          }
+          it { is_expected.to contain_user('galaxy').with(
+            'ensure'     => 'present',
+            'comment'    => 'Galaxy Server',
+            'home'       => '/opt/galaxy',
+            'managehome' => 'true',
+            'system'     => 'true',
+          ) }
+          it { is_expected.to_not contain_user('gxcode') }
+          it { is_expected.to contain_file('/var/opt/galaxy').with(
+            'ensure' => 'directory',
+            'owner'  => 'galaxy',
+          ) }
+        end
+
+        context "galaxy class with galaxy_base_dir set to /srv/galaxy" do
+          let(:params){
+            {
+              :galaxy_base_dir => '/srv/galaxy',
+            }
+          }
+          it { is_expected.to contain_user('gxcode').with_home('/srv/galaxy') }
+        end
+
+        context "galaxy class with galaxy_runtime_dir set to /usr/local/var/galaxy" do
+          let(:params){
+            {
+              :galaxy_runtime_dir => '/usr/local/var/galaxy',
+            }
+          }
+          it { is_expected.to contain_user('galaxy').with_home('/usr/local/var/galaxy') }
         end
       end
     end

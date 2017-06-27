29be3b350795c094e45c1d3ec154c06d678632a2
Jeffrey Miller <millerjl1701@gmail.com>
2017-06-27 11:40:16 +0200

    init.pp: removed periods at end of params to allow for clarity with paths; added param galaxy_manage_venv; param doc for galaxy_venv_dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,15 @@
 #
 # This is the main public class for the galaxy module.
 #
-# @param galaxy_base_dir [Stdlib::Absolutepath] Directory where the galaxy will be installed and configured. Default value: /opt/galaxy.
-# @param galaxy_code_owner [String] Name of the user for that will own the galaxy code directory. Default value: gxcode.
-# @param galaxy_runtime_dir [Stdlib::Absolutepath] Directory for runtime files for the galaxy server. Default value: /var/opt/galaxy.
-# @param galaxy_runtime_user [String] User account name that is the galaxy runtime user. Default value: galaxy.
-# @param galaxy_manage_git [Boolean] Whether to manage the git package resource. Default value: true.
-# @param galaxy_use_separate_users [Boolean] Whether to use separate user accounts for the galaxy code directory and the galaxy runtime. Default value: true.
-# @param git_package_name [String] Specifies the name of the git package to manage. Default value: git.
+# @param galaxy_base_dir [Stdlib::Absolutepath] Directory where the galaxy will be installed and configured. Default value: /opt/galaxy
+# @param galaxy_code_owner [String] Name of the user for that will own the galaxy code directory. Default value: gxcode
+# @param galaxy_manage_git [Boolean] Whether to manage the git package resource. Default value: true
+# @param galaxy_manage_venv [Boolean] Whether to manage creation of the python 2.7 virtual environment for galaxy. Default value: true
+# @param galaxy_runtime_dir [Stdlib::Absolutepath] Directory for runtime files for the galaxy server. Default value: /var/opt/galaxy
+# @param galaxy_runtime_user [String] User account name that is the galaxy runtime user. Default value: galaxy
+# @param galaxy_use_separate_users [Boolean] Whether to use separate user accounts for the galaxy code directory and the galaxy runtime. Default value: true
+# @param galaxy_venv_dir [Stdlib::Absolutepath] Directory where the galaxy python virtual environment lives. Default value: /opt/galaxy/server/.venv
+# @param git_package_name [String] Specifies the name of the git package to manage. Default value: git
 #
 class galaxy (
   Stdlib::Absolutepath  $galaxy_base_dir             = '/opt/galaxy',
@@ -32,6 +34,7 @@ class galaxy (
   String                $galaxy_repo_url             = 'https://github.com/galaxyproject/galaxy.git',
   Boolean               $galaxy_manage_git           = true,
   String                $git_package_name            = 'git',
+  Boolean               $galaxy_manage_venv          = true,
   Boolean               $galaxy_manage_clone         = true,
   Boolean               $galaxy_force_checkout       = false,
   Boolean               $galaxy_manage_download      = false,

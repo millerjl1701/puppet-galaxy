ce9adbba95461c73d0bff52db08684a1ad22a445
Jeffrey Miller <millerjl1701@gmail.com>
2017-06-27 10:45:46 +0200

    added class parameters for galaxy_base_dir, galaxy_runtime_dir, galaxy_use_separate_users; added puppet strings documentation code; rearranged parameters for readability based on the default layout of directories

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,31 @@
+# @api private
 # Class: galaxy
 # ===========================
 #
 # This is the main public class for the galaxy module.
 #
-# Parameters
-# ----------
-#
-# * `sample parameter`
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# @param galaxy_base_dir [Stdlib::Absolutepath] Directory where the galaxy will be installed and configured. Default value: /opt/galaxy.
+# @param galaxy_code_owner [String] Name of the user for that will own the galaxy code directory. Default value: gxcode.
+# @param galaxy_runtime_dir [Stdlib::Absolutepath] Directory for runtime files for the galaxy server. Default value: /var/opt/galaxy.
+# @param galaxy_runtime_user [String] User account name that is the galaxy runtime user. Default value: galaxy.
+# @param galaxy_manage_git [Boolean] Whether to manage the git package resource. Default value: true.
+# @param galaxy_use_separate_users [Boolean] Whether to use separate user accounts for the galaxy code directory and the galaxy runtime. Default value: true.
+# @param git_package_name [String] Specifies the name of the git package to manage. Default value: git.
 #
 class galaxy (
-  Stdlib::Absolutepath  $galaxy_server_dir           = '/opt/galaxy/server',
+  Stdlib::Absolutepath  $galaxy_base_dir             = '/opt/galaxy',
   Stdlib::Absolutepath  $galaxy_config_dir           = '/opt/galaxy/config',
   Stdlib::Absolutepath  $galaxy_config_file          = '/opt/galaxy/config/galaxy.ini',
-  Stdlib::Absolutepath  $galaxy_mutable_config_dir   = '/var/opt/galaxy/config',
+  Stdlib::Absolutepath  $galaxy_requirements_file    = '/opt/galaxy/server/lib/galaxy/dependencies/pinned-requirements.txt',
+  Stdlib::Absolutepath  $galaxy_server_dir           = '/opt/galaxy/server',
+  Stdlib::Absolutepath  $galaxy_shed_tools_dir       = '/opt/galaxy/shed_tools',
   Stdlib::Absolutepath  $galaxy_tool_dependency_dir  = '/opt/galaxy/server/dependencies',
   Stdlib::Absolutepath  $galaxy_venv_dir             = '/opt/galaxy/server/.venv',
-  Stdlib::Absolutepath  $galaxy_mutable_data_dir     = '/var/opt/galaxy/data',
-  Stdlib::Absolutepath  $galaxy_shed_tools_dir       = '/opt/galaxy/shed_tools',
+  Stdlib::Absolutepath  $galaxy_runtime_dir          = '/var/opt/galaxy',
+  Stdlib::Absolutepath  $galaxy_mutable_config_dir   = '/var/opt/galaxy/config',
   Stdlib::Absolutepath  $galaxy_shed_tool_conf_file  = '/var/opt/galaxy/config/shed_tool_conf.xml',
-  Stdlib::Absolutepath  $galaxy_requirements_file    = '/opt/galaxy/server/lib/galaxy/dependencies/pinned-requirements.txt',
+  Stdlib::Absolutepath  $galaxy_mutable_data_dir     = '/var/opt/galaxy/data',
+  Boolean               $galaxy_use_separate_users   = true,
   String                $galaxy_code_owner           = 'gxcode',
   String                $galaxy_runtime_user         = 'galaxy',
   String                $galaxy_commit_id            = 'release_17.05',

b82bf081389f6644fef270350b3baadf32a5ce5f
Jeffrey Miller <millerjl1701@gmail.com>
2017-06-27 13:54:52 +0200

    spec tests: added tests for the installation of galaxy

--- spec/classes/galaxy_spec.rb
+++ spec/classes/galaxy_spec.rb
@@ -21,19 +21,44 @@ describe 'galaxy' do
             'ensure'     => 'present',
             'comment'    => 'Galaxy Code',
             'home'       => '/opt/galaxy',
-            'managehome' => 'true',
+            'managehome' => 'false',
             'system'     => 'true',
+          ) }
+
+          it { is_expected.to contain_file('/opt/galaxy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'gxcode',
+            'group'   => 'gxcode',
+            'mode'    => '0755',
+            'require' => 'User[gxcode]',
           ) }
 
           it { is_expected.to contain_user('galaxy').with(
             'ensure'     => 'present',
             'comment'    => 'Galaxy Server',
             'home'       => '/var/opt/galaxy',
-            'managehome' => 'true',
+            'managehome' => 'false',
             'system'     => 'true',
           ) }
 
+          it { is_expected.to contain_file('/var/opt/galaxy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'galaxy',
+            'group'   => 'galaxy',
+            'mode'    => '0755',
+            'require' => 'User[galaxy]',
+          ) }
+
           it { is_expected.to contain_package('git').with_ensure('present') }
+
+          it { is_expected.to contain_vcsrepo('/opt/galaxy/server').with(
+            'ensure'     => 'present',
+            'provider'   => 'git',
+            'source'     => 'https://github.com/galaxyproject/galaxy.git',
+            'revision'   => 'release_17.05',
+            'user'       => 'gxcode',
+            'submodules' => 'false',
+          ) }
         end
 
         context "galaxy class with galaxy_manage_git set to false" do
@@ -55,14 +80,25 @@ describe 'galaxy' do
             'ensure'     => 'present',
             'comment'    => 'Galaxy Server',
             'home'       => '/opt/galaxy',
-            'managehome' => 'true',
+            'managehome' => 'false',
             'system'     => 'true',
           ) }
           it { is_expected.to_not contain_user('gxcode') }
-          it { is_expected.to contain_file('/var/opt/galaxy').with(
-            'ensure' => 'directory',
-            'owner'  => 'galaxy',
+          it { is_expected.to contain_file('/opt/galaxy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'galaxy',
+            'group'   => 'galaxy',
+            'mode'    => '0755',
+            'require' => 'User[galaxy]',
           ) }
+          it { is_expected.to contain_file('/var/opt/galaxy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'galaxy',
+            'group'   => 'galaxy',
+            'mode'    => '0755',
+            'require' => 'User[galaxy]',
+          ) }
+          it { is_expected.to contain_vcsrepo('/opt/galaxy/server').with_user('galaxy') }
         end
 
         context "galaxy class with galaxy_base_dir set to /srv/galaxy" do
@@ -72,6 +108,13 @@ describe 'galaxy' do
             }
           }
           it { is_expected.to contain_user('gxcode').with_home('/srv/galaxy') }
+          it { is_expected.to contain_file('/srv/galaxy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'gxcode',
+            'group'   => 'gxcode',
+            'mode'    => '0755',
+            'require' => 'User[gxcode]',
+          ) }
         end
 
         context "galaxy class with galaxy_runtime_dir set to /usr/local/var/galaxy" do
@@ -81,6 +124,40 @@ describe 'galaxy' do
             }
           }
           it { is_expected.to contain_user('galaxy').with_home('/usr/local/var/galaxy') }
+          it { is_expected.to contain_file('/usr/local/var/galaxy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'galaxy',
+            'group'   => 'galaxy',
+            'mode'    => '0755',
+            'require' => 'User[galaxy]',
+          ) }
+        end
+
+        context "galaxy class with galaxy_server_dir set to /usr/local/galaxy/server" do
+          let(:params){
+            {
+              :galaxy_server_dir => '/usr/local/galaxy/server',
+            }
+          }
+          it { is_expected.to contain_vcsrepo('/usr/local/galaxy/server') }
+        end
+
+        context "galaxy class with galaxy_repo_url set to http://localhost/galaxy.git" do
+          let(:params){
+            {
+              :galaxy_repo_url => 'http://localhost/galaxy.git',
+            }
+          }
+          it { is_expected.to contain_vcsrepo('/opt/galaxy/server').with_source('http://localhost/galaxy.git') }
+        end
+
+        context "galaxy class with galaxy_commit_id set to SHA 1 hash" do
+          let(:params){
+            {
+              :galaxy_commit_id => '9dca211e2fe59eaad1f7b95c8d51608b2afa05af',
+            }
+          }
+          it { is_expected.to contain_vcsrepo('/opt/galaxy/server').with_revision('9dca211e2fe59eaad1f7b95c8d51608b2afa05af') }
         end
       end
     end

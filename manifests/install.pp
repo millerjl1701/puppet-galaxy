169cc5b493fb9545564f996d9cb5e6687d99867c
Jeffrey Miller <millerjl1701@gmail.com>
2017-06-27 13:54:11 +0200

    install: added code to clone the galaxy repo with the specified galaxy_commit_id to be owned by either gxcode or galaxy depending on the galaxy_use_separate_users parameters

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,26 @@
 # @api private
 # == Class galaxy::install
 #
-# This private class is called from galaxy for install.
+# @param _repo_owner [String] Determines which user account should own the code repo directory based on the value of the boolean galaxy_use_separate_users. Default: gxcode
 #
 class galaxy::install {
+  if $::galaxy::galaxy_use_separate_users {
+    $_repo_owner = $::galaxy::galaxy_code_owner
+  }
+  else {
+    $_repo_owner = $::galaxy::galaxy_runtime_user
+  }
 
+  # installation of the galaxy code from the git repository
+  # ToDo: need to figure out a way for notification of galaxy server
+  #   processes to restart when the current release id installed on the system
+  #   does not match the galaxy_commit_id
+  vcsrepo { $::galaxy::galaxy_server_dir:
+    ensure     => present,
+    provider   => git,
+    source     => $::galaxy::galaxy_repo_url,
+    revision   => $::galaxy::galaxy_commit_id,
+    user       => $_repo_owner,
+    submodules => false,
+  }
 }
